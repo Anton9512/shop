2c043a9c3ed712becd9b3d6b0286591c7f6092c5
Anton9512 <anton.mail9512@gmail.com>
2022-04-30 15:53:48 +0300

    show-hide shoppingPage on click

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -1,23 +1,34 @@
 class Shopping {
+  handleClear() {
+    ROOT_SHOPPING.innerHTML = "";
+  }
+
   render() {
     const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = "";
+    let sumCatalog = 0;
 
     CATALOG.forEach(({ id, name, price }) => {
       if (productsStore.indexOf(id) !== -1) {
         htmlCatalog += `
             <tr>
-               <td>⚡️ ${name}</td>
-               <td>${price.toLocaleString()} USD</td>
+               <td class="shopping-element__name">⚡️ ${name}</td>
+               <td class="shopping-element__price">${price.toLocaleString()} USD</td>
             </tr>
          `;
+        sumCatalog += price;
       }
     });
 
     const html = `
-      <div>
+      <div class="shopping-container">
+         <div class="shopping__close" onclick="shoppingPage.handleClear()"></div>
          <table>
             ${htmlCatalog}
+            <tr>
+               <td class="shopping-element__name">💥 Сумма:</td>
+               <td class="shopping-element__price">${sumCatalog.toLocaleString()} USD</td>
+         </tr>
          </table>
       </div>
     `;
